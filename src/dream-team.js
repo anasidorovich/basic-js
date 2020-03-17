5b65ab0683e741c75e06711f1ecbf6c6f04b0ed4
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2020-03-17 16:47:17 +0300

    feat: add function that returns name of a newly made team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    return members
+      .filter(member => typeof member === 'string')
+      .map(member => member.trim().charAt(0).toUpperCase())
+      .sort()
+      .join('');
+  }
+  return false;
 };
