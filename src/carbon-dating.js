29bb956c77f93296b84ac1e8e497771b506c8fab
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2020-03-17 16:57:13 +0300

    feat: add function to determine the age of archaeological finds

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,22 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const RADIOACTIVE_DECAY = 0.693;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(num) {
+  if (typeof num !== 'string') {
+    return false;
+  }
+  let nums = num.split(".");
+  if (nums.length > 2) {
+    num = nums[0] + "." + nums[1];
+  }
+  if (isNaN(num)) {
+    return false;
+  }
+  num = parseFloat(num);
+  if (num <= 0 || num > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  return Math.floor((Math.log(MODERN_ACTIVITY / num) * HALF_LIFE_PERIOD / RADIOACTIVE_DECAY)) + 1;
 };
