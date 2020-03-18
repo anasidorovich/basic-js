1e9c17cd7162aa10f53cb5634b385f8d7e8f270d
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2020-03-18 16:42:50 +0300

    feat: implement the function that returns the time of the year

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,11 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+var SEASONS = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter"];
+
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  if (Object.prototype.toString.call(date) !== '[object Date]') {
+    throw new Error();
+  }
+  return SEASONS[date.getMonth()];
 };
