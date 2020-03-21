d302d77dec4dd17cc9181c8ada1d72997fdba728
sidarova <anasidorovich@gmail.com>
2020-03-21 22:05:25 +0300

    feat:  implement the function-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,13 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let repeatTimes  = options.repeatTimes,
+        separator = options.separator || '+',
+        addition = options.addition !== undefined ? '' + options.addition : '',
+        additionStr = '',
+        additionRepeatTimes = options.additionRepeatTimes || 1,
+        additionSeparator = options.additionSeparator || '';
+
+    if (additionRepeatTimes > 0) {
+        additionStr = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+    }
+    return repeatTimes > 0 ? (str + additionStr + separator).repeat(repeatTimes - 1) + str + additionStr : str + additionStr;
 };
-  
